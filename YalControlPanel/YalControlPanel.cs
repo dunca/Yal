8ec58df37f92e6da978ab485104048edf8729fe5
flvdnc <flv@mail.com>
2016-09-13 16:08:25 +0300

    Included support for Windows 10 in YCP plugin

--- YalControlPanel/YalControlPanel.cs
+++ YalControlPanel/YalControlPanel.cs
@@ -25,61 +25,61 @@ namespace YalControlPanel
         private YalControlPanelUC ControlPanelPluginInstance { get; set; }
         string controlPath = string.Concat(Environment.GetEnvironmentVariable("SYSTEMROOT"), @"\system32\control.exe");
         private string osString = Utils.GetOsVersion();
-        private string[] canonicalNames = @"Action Center;Microsoft.ActionCenter;7,8,8.1
-Administrative Tools;Microsoft.AdministrativeTools;Vista,7,8,8.1
-AutoPlay;Microsoft.AutoPlay;Vista,7,8,8.1
-Biometric Devices;Microsoft.BiometricDevices;7,8,8.1
-BitLocker Drive Encryption;Microsoft.BitLockerDriveEncryption;Vista,7,8,8.1
-Color Management;Microsoft.ColorManagement;Vista,7,8,8.1
-Credential Manager;Microsoft.CredentialManager;7,8,8.1
-Date and Time;Microsoft.DateAndTime;Vista,7,8,8.1
-Default Programs;Microsoft.DefaultPrograms;Vista,7,8,8.1
-Device Manager;Microsoft.DeviceManager;Vista,7,8,8.1
-Devices and Printers;Microsoft.DevicesAndPrinters;7,8,8.1
-Display;Microsoft.Display;7,8,8.1
-Ease of Access Center;Microsoft.EaseOfAccessCenter;Vista,7,8,8.1
-Family Safety;Microsoft.ParentalControls;Vista,7,8,8.1
-File History;Microsoft.FileHistory;8,8.1
-Folder Options;Microsoft.FolderOptions;Vista,7,8,8.1
-Fonts;Microsoft.Fonts;Vista,7,8,8.1
-HomeGroup;Microsoft.HomeGroup;7,8,8.1
-Indexing Options;Microsoft.IndexingOptions;Vista,7,8,8.1
-Infrared;Microsoft.Infrared;7,8,8.1
-Internet Options;Microsoft.InternetOptions;Vista,7,8,8.1
-iSCSI Initiator;Microsoft.iSCSIInitiator;Vista,7,8,8.1
-iSNS Server;Microsoft.iSNSServer;Vista,7,8,8.1
-Keyboard;Microsoft.Keyboard;Vista,7,8,8.1
-Language;Microsoft.Language;8,8.1
-Location Settings;Microsoft.LocationSettings;8,8.1
-Mouse;Microsoft.Mouse;Vista,7,8,8.1
-MPIOConfiguration;Microsoft.MPIOConfiguration;7,8,8.1
-Network and Sharing Center;Microsoft.NetworkAndSharingCenter;Vista,7,8,8.1
-Notification Area Icons;Microsoft.NotificationAreaIcons;7,8,8.1
-Pen and Touch;Microsoft.PenAndTouch;7,8,8.1
-Personalization;Microsoft.Personalization;Vista,7,8,8.1
-Phone and Modem;Microsoft.PhoneAndModem;7,8,8.1
-Power Options;Microsoft.PowerOptions;Vista,7,8,8.1
-Programs and Features;Microsoft.ProgramsAndFeatures;Vista,7,8,8.1
-Recovery;Microsoft.Recovery;7,8,8.1
-Region;Microsoft.RegionAndLanguage;7,8,8.1
-RemoteApp and Desktop Connections;Microsoft.RemoteAppAndDesktopConnections;7,8,8.1
-Sound;Microsoft.Sound;7,8,8.1
-Speech Recognition;Microsoft.SpeechRecognition;7,8,8.1
-Storage Spaces;Microsoft.StorageSpaces;8,8.1
-Sync Center;Microsoft.SyncCenter;Vista,7,8,8.1
-System;Microsoft.System;Vista,7,8,8.1
-Tablet PC Settings;Microsoft.TabletPCSettings;Vista,7,8,8.1
-Taskbar and Navigation;Microsoft.Taskbar;8,8.1
-Troubleshooting;Microsoft.Troubleshooting;7,8,8.1
-TSAppInstall;Microsoft.TSAppInstall;7,8,8.1
-User Accounts;Microsoft.UserAccounts;Vista,7,8,8.1
-Windows Anytime Upgrade;Microsoft.WindowsAnytimeUpgrade;Vista,7,8,8.1
-Windows Defender;Microsoft.WindowsDefender;Vista,7,8,8.1
-Windows Firewall;Microsoft.WindowsFirewall;Vista,7,8,8.1
-Windows Mobility Center;Microsoft.MobilityCenter;Vista,7,8,8.1
-Windows To Go;Microsoft.PortableWorkspaceCreator;8,8.1
-Windows Update;Microsoft.WindowsUpdate;Vista,7,8,8.1
-Work Folders;Microsoft.WorkFolders;8.1
+        private string[] canonicalNames = @"Action Center;Microsoft.ActionCenter;7,8,8.1,10
+Administrative Tools;Microsoft.AdministrativeTools;Vista,7,8,8.1,10
+AutoPlay;Microsoft.AutoPlay;Vista,7,8,8.1,10
+Biometric Devices;Microsoft.BiometricDevices;7,8,8.1,10
+BitLocker Drive Encryption;Microsoft.BitLockerDriveEncryption;Vista,7,8,8.1,10
+Color Management;Microsoft.ColorManagement;Vista,7,8,8.1,10
+Credential Manager;Microsoft.CredentialManager;7,8,8.1,10
+Date and Time;Microsoft.DateAndTime;Vista,7,8,8.1,10
+Default Programs;Microsoft.DefaultPrograms;Vista,7,8,8.1,10
+Device Manager;Microsoft.DeviceManager;Vista,7,8,8.1,10
+Devices and Printers;Microsoft.DevicesAndPrinters;7,8,8.1,10
+Display;Microsoft.Display;7,8,8.1,10
+Ease of Access Center;Microsoft.EaseOfAccessCenter;Vista,7,8,8.1,10
+Family Safety;Microsoft.ParentalControls;Vista,7,8,8.1,10
+File History;Microsoft.FileHistory;8,8.1,10
+Folder Options;Microsoft.FolderOptions;Vista,7,8,8.1,10
+Fonts;Microsoft.Fonts;Vista,7,8,8.1,10
+HomeGroup;Microsoft.HomeGroup;7,8,8.1,10
+Indexing Options;Microsoft.IndexingOptions;Vista,7,8,8.1,10
+Infrared;Microsoft.Infrared;7,8,8.1,10
+Internet Options;Microsoft.InternetOptions;Vista,7,8,8.1,10
+iSCSI Initiator;Microsoft.iSCSIInitiator;Vista,7,8,8.1,10
+iSNS Server;Microsoft.iSNSServer;Vista,7,8,8.1,10
+Keyboard;Microsoft.Keyboard;Vista,7,8,8.1,10
+Language;Microsoft.Language;8,8.1,10
+Location Settings;Microsoft.LocationSettings;8,8.1,10
+Mouse;Microsoft.Mouse;Vista,7,8,8.1,10
+MPIOConfiguration;Microsoft.MPIOConfiguration;7,8,8.1,10
+Network and Sharing Center;Microsoft.NetworkAndSharingCenter;Vista,7,8,8.1,10
+Notification Area Icons;Microsoft.NotificationAreaIcons;7,8,8.1,10
+Pen and Touch;Microsoft.PenAndTouch;7,8,8.1,10
+Personalization;Microsoft.Personalization;Vista,7,8,8.1,10
+Phone and Modem;Microsoft.PhoneAndModem;7,8,8.1,10
+Power Options;Microsoft.PowerOptions;Vista,7,8,8.1,10
+Programs and Features;Microsoft.ProgramsAndFeatures;Vista,7,8,8.1,10
+Recovery;Microsoft.Recovery;7,8,8.1,10
+Region;Microsoft.RegionAndLanguage;7,8,8.1,10
+RemoteApp and Desktop Connections;Microsoft.RemoteAppAndDesktopConnections;7,8,8.1,10
+Sound;Microsoft.Sound;7,8,8.1,10
+Speech Recognition;Microsoft.SpeechRecognition;7,8,8.1,10
+Storage Spaces;Microsoft.StorageSpaces;8,8.1,10
+Sync Center;Microsoft.SyncCenter;Vista,7,8,8.1,10
+System;Microsoft.System;Vista,7,8,8.1,10
+Tablet PC Settings;Microsoft.TabletPCSettings;Vista,7,8,8.1,10
+Taskbar and Navigation;Microsoft.Taskbar;8,8.1,10
+Troubleshooting;Microsoft.Troubleshooting;7,8,8.1,10
+TSAppInstall;Microsoft.TSAppInstall;7,8,8.1,10
+User Accounts;Microsoft.UserAccounts;Vista,7,8,8.1,10
+Windows Anytime Upgrade;Microsoft.WindowsAnytimeUpgrade;Vista,7,8,8.1,10
+Windows Defender;Microsoft.WindowsDefender;Vista,7,8,8.1,10
+Windows Firewall;Microsoft.WindowsFirewall;Vista,7,8,8.1,10
+Windows Mobility Center;Microsoft.MobilityCenter;Vista,7,8,8.1,10
+Windows To Go;Microsoft.PortableWorkspaceCreator;8,8.1,10
+Windows Update;Microsoft.WindowsUpdate;Vista,7,8,8.1,10
+Work Folders;Microsoft.WorkFolders;8.1,10
 ".Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
 
         public YalControlPanel()
