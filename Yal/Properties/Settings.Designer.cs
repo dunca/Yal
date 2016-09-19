34d7cc4b46e32acf73b92d219d5be368b769aedc
flvdnc <flv@mail.com>
2016-09-19 21:21:27 +0300

    Changed the default number of items to display

--- Yal/Properties/Settings.Designer.cs
+++ Yal/Properties/Settings.Designer.cs
@@ -211,7 +211,7 @@ namespace Yal.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("10")]
+        [global::System.Configuration.DefaultSettingValueAttribute("15")]
         public int MaxItems {
             get {
                 return ((int)(this["MaxItems"]));
@@ -223,7 +223,7 @@ namespace Yal.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("5")]
+        [global::System.Configuration.DefaultSettingValueAttribute("10")]
         public int MaxVisible {
             get {
                 return ((int)(this["MaxVisible"]));
@@ -320,7 +320,7 @@ namespace Yal.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        [global::System.Configuration.DefaultSettingValueAttribute("5")]
         public int MaxPluginItems {
             get {
                 return ((int)(this["MaxPluginItems"]));
