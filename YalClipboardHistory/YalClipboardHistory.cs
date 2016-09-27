b0022961d1d5cc6f07e311e72b73499170e98429
flvdnc <flv@mail.com>
2016-09-27 16:28:34 +0300

    Removed some unused namespaces

--- YalClipboardHistory/YalClipboardHistory.cs
+++ YalClipboardHistory/YalClipboardHistory.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Linq;
 using System.Drawing;
+using System.Windows.Forms;
 
 using Utilities;
 using PluginInterfaces;
--- YalProcessKiller/YalProcessKiller.cs
+++ YalProcessKiller/YalProcessKiller.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Drawing;
 using System.Diagnostics;
 using System.Windows.Forms;
-using System.Collections.Generic;
 
 using Utilities;
 using PluginInterfaces;
