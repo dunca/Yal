ce2089d7359d9d55ed221d67477e9cd03fdcf298
flvdnc <flv@mail.com>
2016-10-03 11:12:04 +0300

    Yal: don't show the app in the taskbar

--- Yal/Yal.Designer.cs
+++ Yal/Yal.Designer.cs
@@ -121,6 +121,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Yal";
+            this.ShowInTaskbar = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Yal";
